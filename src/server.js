444a8aacc6d742ab9e05124cac13cf915eaf8cab
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 16:38:05 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Point Focal Backend V9 running on port ${PORT}`);
+  console.log(`Point Focal Backend V9 running on port ${PORT}`);
 });
