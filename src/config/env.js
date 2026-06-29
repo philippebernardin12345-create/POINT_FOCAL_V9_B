6be83640a3f8c316e241357bcd387ae5b64b1d0c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 16:47:04 +0100

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,1 +1,8 @@
-ok
+require("dotenv").config();
+
+module.exports = {
+  port: process.env.PORT || 5000,
+  databaseUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET || "CHANGE_ME_SECRET",
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d"
+};
