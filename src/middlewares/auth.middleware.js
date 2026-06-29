66f807da88da04da65cf4ac81b31f8b42093ce7c
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 17:02:04 +0100

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,1 +1,27 @@
-ok
+const { verifyToken } = require("../config/jwt");
+
+function authMiddleware(req, res, next) {
+  try {
+    const header = req.headers.authorization;
+
+    if (!header || !header.startsWith("Bearer ")) {
+      return res.status(401).json({
+        success: false,
+        message: "Token manquant."
+      });
+    }
+
+    const token = header.split(" ")[1];
+    const decoded = verifyToken(token);
+
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Token invalide ou expiré."
+    });
+  }
+}
+
+module.exports = authMiddleware;
