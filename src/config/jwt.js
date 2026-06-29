69cafdee00ad33c25324b2aa7cbf4a5736af59ec
philippebernardin12345-create <philippebernardin12345@gmail.com>
2026-06-29 16:47:41 +0100

    Update jwt.js

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,1 +1,17 @@
-ok
+const jwt = require("jsonwebtoken");
+const env = require("./env");
+
+function signToken(payload) {
+  return jwt.sign(payload, env.jwtSecret, {
+    expiresIn: env.jwtExpiresIn
+  });
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, env.jwtSecret);
+}
+
+module.exports = {
+  signToken,
+  verifyToken
+};
